24ad21956c89b435bf7c5c5f08e0adc3d287a2d6
Steve Richards <steverichards80@hotmail.com>
2023-03-04 18:45:23 +0000

    Commit initial files. Psuedo code + first function to get computer choice for the game

---
+++ javascript.js
@@ -0,0 +1,16 @@
+/*getComputerChoice - randomly return rock paper or scissors
+compare player selection and computerselection in a single round of the game
+Return a string that declares the winner else.g. "you win, rock beats scissors!"
+
+Function called game will play a 5 round game and keep score */
+
+
+
+const compChoices = ["rock", "paper", "scissors"];
+
+function getComputerChoice(Choice) {
+
+let compSelection = compChoices[Math.floor(Math.random() * compChoices.length)];
+return compSelection;
+
+}
