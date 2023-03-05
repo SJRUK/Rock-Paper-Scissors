97482f68a355fe40d952846499a4fe3f66d966e0
Steve Richards <steverichards80@hotmail.com>
2023-03-05 16:27:30 +0000

    Added playRound function

--- javascript.js
+++ javascript.js
@@ -11,23 +11,59 @@ const compChoices = ["rock", "paper", "scissors"];
 function getComputerChoice(Choice) {
 
 let compSelection = compChoices[Math.floor(Math.random() * compChoices.length)];
-console.log("Computer chooses: " + compSelection);
 return compSelection;
 }
-
-getComputerChoice(compChoices);
 
 
 function getPlayerChoice(Choice){
     
-let playerChoice = prompt("Choose rock, paper or scissors");
+let playerChoice = prompt("Please make your choice: rock, paper or scissors");
 
 let playerSelection = playerChoice.toLowerCase();
-console.log("You choose: " + playerSelection);
 return playerSelection;
 }
 
-getPlayerChoice();
+
+function playRound(computersGo, playersGo){
+    let VS = computersGo + " VS " + playersGo;
+    console.log(VS);
+       if (computersGo === playersGo){
+        return "It's a draw!";
+       }
+       else if (computersGo === "rock" && playersGo === "paper") {
+        return "Player wins - paper beats rock!";
+       }
+        else if (computersGo === "paper" && playersGo === "rock") {
+          return "Computer wins - paper beats rock!";
+        }
+        else if (computersGo === "scissors" && playersGo === "rock") {
+            return "Player wins - rock beats scissors!";
+          }
+          else if (computersGo === "rock" && playersGo === "scissors") {
+            return "Computer wins - rock beats scissors!"; 
+        }
+        else if (computersGo === "paper" && playersGo === "scissors") {
+            return "Player wins - scissors beats paper!";
+          }
+          else if (computersGo === "scissors" && playersGo === "paper") {
+            return "Computer wins - scissors beats paper!";
+          }  
+          else {
+            return "You did not choose rock, paper or scissors! Please try again.";
+          }
+
+    }
+        
+    
+
+
+const computersGo = getComputerChoice();
+const playersGo = getPlayerChoice();
+
+console.log("Computer chooses: " + computersGo);
+console.log("You choose: " + playersGo);
+
+console.log(playRound(computersGo, playersGo));
 
 
 
