839da703abbde448810b7ae77ae55ba289491c8a
Steve Richards <steverichards80@hotmail.com>
2023-03-10 19:14:59 +0000

    created filed with 2 after to work on code that will play the game 5 times

--- javascript2.js
+++ javascript2.js
@@ -18,75 +18,101 @@ let playerSelection = playerChoice.toLowerCase();
 return playerSelection;
 }
 
+let computersGo;
+let playersGo;
 
 function playRound(computersGo, playersGo){
-    let VS = computersGo + " VS " + playersGo;
+  computersGo = getComputerChoice();
+  playersGo = getPlayerChoice();
+    let VS = "Player: " + playersGo + " VS " + computersGo + " :Computer";
     console.log(VS);
        if (computersGo === playersGo){
+        pWin = false;
+        cWin = false;
         return "It's a draw!";
        }
        else if (computersGo === "rock" && playersGo === "paper") {
-        return "Player wins - paper beats rock!";
+            pWin = true;
+            cWin =false;
+            return "Player wins - rock beats paper!";
        }
         else if (computersGo === "paper" && playersGo === "rock") {
-          return "Computer wins - paper beats rock!";
+            cWin = true;
+            pWin = false;
+            return "Computer wins - paper beats rock!";
         }
         else if (computersGo === "scissors" && playersGo === "rock") {
+            pWin = true;
+            cWin =false;
             return "Player wins - rock beats scissors!";
-          }
+        }
           else if (computersGo === "rock" && playersGo === "scissors") {
+            cWin = true;
+            pWin = false;
             return "Computer wins - rock beats scissors!"; 
         }
         else if (computersGo === "paper" && playersGo === "scissors") {
+            pWin = true;
+            cWin =false;
             return "Player wins - scissors beats paper!";
           }
           else if (computersGo === "scissors" && playersGo === "paper") {
+            cWin = true;
+            pWin = false;
             return "Computer wins - scissors beats paper!";
           }  
           else {
+            pWin = false;
+            cWin = false;
             return "You did not choose rock, paper or scissors! Please try again.";
           }
     }
         
-  
-const computersGo = getComputerChoice();
-const playersGo = getPlayerChoice();
-
-console.log("Computer chooses: " + computersGo);
-console.log("You choose: " + playersGo);
-
-console.log(playRound(computersGo, playersGo));
-
-/*
-
 let playerScore = 0;
 let computerScore = 0;
 
+let pWin = new Boolean(false);
+let cWin = new Boolean(false);
 
-function game {
-  for (let i = 0; i < 5; i++){
-    playRound(computersGo, playersGo);
-    if (pWin = true){
+function game() {
+    let gamesPlayed = 0;
+    
+    while (gamesPlayed < 5){
+        (playRound(computersGo, playersGo));
+
+    if (pWin){
     playerScore++;
+    console.log("PLAYER WINS THIS ROUND! Player " + playerScore + " Vs " + computerScore + " Computer");
     }
-    else if (cWin = true) {
-      computerScore++;
+    else if (cWin) {
+    computerScore++;
+    console.log("COMPUTER WINS THIS ROUND! Player " + playerScore + " Vs " + computerScore + " Computer");
     }
-  }
-}
+    else {
+      console.log("This round is a draw!" + " Player " + playerScore + " Vs " + computerScore + " Computer")
+    }
+    
+  gamesPlayed++;
+
+  if (gamesPlayed >= 5) {
+    console.log("Game Over!" + " Player " + playerScore + " Vs " + computerScore + " Computer");
+   }
+}}
+
+console.log(game());
 
 
-*/
 
 
 
 
 
-/*if (playerSelection == "rock" || "paper" || "scissors"){
+/*
+if (playerSelection == "rock" || "paper" || "scissors"){
 
 console.log("You choose: " + playerSelection);
 return playerSelection;
 }
 else {
-    console.log("That was not rock, paper or scissors!");
-}*/
+    console.log("That was not rock, paper or scissors!");} 
+*/
