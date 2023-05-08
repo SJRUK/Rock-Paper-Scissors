f1f345e7dc8e5d1c2d733e387a9f58e9318d94eb
Steve Richards <steverichards80@hotmail.com>
2023-05-08 16:00:21 +0100

    DOM changes, mainly CSS

--- javascript.js
+++ javascript.js
@@ -48,10 +48,16 @@ container2.appendChild(result);
 const score = document.createElement('h3');
 container2.appendChild(score);
 
-
+function reloadPage()
+{
+location.reload();
+}
 
 
 function playRound(computersGo, playersGo){
+  
+  if (computerScore < 5 && playerScore < 5){
+  
   computersGo = getComputerChoice();
 
   
@@ -59,12 +65,13 @@ function playRound(computersGo, playersGo){
   
   
     versus.textContent = "(Player) " + playersGo + " VS " + computersGo + " (Computer)";
+    versus.style.border = "2px dotted black";
+    versus.style.padding = "20px";
+    versus.style.borderRadius = "20px";
     console.log(versus);
 
 
        if (computersGo === playersGo){
-        pWin = false;
-        cWin = false; 
         const draw = document.createElement('p');
         draw.classList.add('draw');
         draw.textContent = "It's a draw!";
@@ -72,11 +79,14 @@ function playRound(computersGo, playersGo){
         playerScore++;
         computerScore++;
         score.textContent = "Player's Score = " + playerScore + " Computer's Score = " + computerScore;
+        
        }
        else if (computersGo === "rock" && playersGo === "paper") {
         const pWin = document.createElement('p');
         pWin.classList.add('pWin');
         pWin.textContent = "Player wins!";
+        pWin.style.color = "green";
+        pWin.style.fontWeight = "900";
         result.appendChild(pWin);
         playerScore++;
         score.textContent = "Player's Score = " + playerScore + " Computer's Score = " + computerScore;
@@ -86,6 +96,8 @@ function playRound(computersGo, playersGo){
           const cWin = document.createElement('p');
           cWin.classList.add('pWin');
           cWin.textContent = "Computer wins!";
+          cWin.style.color = "red";
+          cWin.style.fontWeight = "900";
           result.appendChild(cWin);
           computerScore++;
           score.textContent = "Player's Score = " + playerScore + " Computer's Score = " + computerScore;
@@ -94,6 +106,8 @@ function playRound(computersGo, playersGo){
           const pWin = document.createElement('p');
           pWin.classList.add('pWin');
           pWin.textContent = "Player wins!";
+          pWin.style.color = "green";
+          pWin.style.fontWeight = "900";
           result.appendChild(pWin);
           playerScore++;
           score.textContent = "Player's Score = " + playerScore + " Computer's Score = " + computerScore;
@@ -102,6 +116,8 @@ function playRound(computersGo, playersGo){
           const cWin = document.createElement('p');
           cWin.classList.add('pWin');
           cWin.textContent = "Computer wins!";
+          cWin.style.color = "red";
+          cWin.style.fontWeight = "900";
           result.appendChild(cWin);
           computerScore++;
           score.textContent = "Player's Score = " + playerScore + " Computer's Score = " + computerScore;
@@ -111,6 +127,8 @@ function playRound(computersGo, playersGo){
           pWin.classList.add('pWin');
           pWin.textContent = "Player wins!";
           result.appendChild(pWin);
+          pWin.style.color = "green";
+          pWin.style.fontWeight = "900";
           playerScore++;
           score.textContent = "Player's Score = " + playerScore + " Computer's Score = " + computerScore;
           }
@@ -118,32 +136,67 @@ function playRound(computersGo, playersGo){
             const cWin = document.createElement('p');
             cWin.classList.add('pWin');
             cWin.textContent = "Computer wins!";
+            cWin.style.color = "red";
+            cWin.style.fontWeight = "900";
             result.appendChild(cWin);
             computerScore++;
             score.textContent = "Player's Score = " + playerScore + " Computer's Score = " + computerScore;
           }  
+        }
+        else {
+          return;
+        }
       if (playerScore >= 5 && computerScore < 5)
       {
-        const pWinner = document.createElement('h2');
+        const pWinner = document.createElement('h1');
         pWinner.classList.add('pWinner');
         pWinner.textContent = "Player wins the game!";
+        pWinner.style.color = "green";
+        pWinner.style.fontWeight = "900";
         container2.appendChild(pWinner);  
+        const refresh = document.createElement('button');
+        refresh.textContent = "Play again?";
+        refresh.style.padding = "20px";
+        refresh.style.borderRadius = "20px";
+        refresh.addEventListener("click", function(){
+          reloadPage();
+        });
+        container2.appendChild(refresh);
+
       }    
 
       if (computerScore >= 5 && playerScore < 5)
       {
-        const cWinner = document.createElement('h2');
+        const cWinner = document.createElement('h1');
         cWinner.classList.add('cWinner');
         cWinner.textContent = "Computer wins the game!";
-        container2.appendChild(cWinner);  
+        cWinner.style.color = "red";
+        cWinner.style.fontWeight = "900";
+        container2.appendChild(cWinner);
+        const refresh = document.createElement('button');
+        refresh.textContent = "Play again?";
+        refresh.style.padding = "20px";
+        refresh.style.borderRadius = "20px";
+        refresh.addEventListener("click", function(){
+          reloadPage();
+        });
+        container2.appendChild(refresh);  
       }    
 
       if (computerScore == 5 && playerScore == 5)
       {
-        const aDraw = document.createElement('h2');
+        const aDraw = document.createElement('h1');
         aDraw.classList.add('aDraw');
         aDraw.textContent = "It's a draw - no-one wins!";
         container2.appendChild(aDraw);  
+        const refresh = document.createElement('button');
+        refresh.textContent = "Play again?";
+        refresh.style.padding = "20px";
+        refresh.style.borderRadius = "20px";
+        refresh.addEventListener("click", function(){
+          reloadPage();
+        });
+        container2.appendChild(refresh);
       }    
 
 
